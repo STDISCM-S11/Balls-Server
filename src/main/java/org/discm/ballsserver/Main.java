9ecc6eb6fd726bc0664dddabca72b16f03a8f841
ayishamiara <75210705+ayishamiara@users.noreply.github.com>
2024-04-05 01:43:08 +0800

    change window

--- src/main/java/org/discm/ballsserver/Main.java
+++ src/main/java/org/discm/ballsserver/Main.java
@@ -33,8 +33,8 @@ public class Main extends Application {
 
     private static final int CANVAS_WIDTH = 1280;
     private static final int CANVAS_HEIGHT = 720;
-    private static final int WINDOW_WIDTH = 1920;
-    private static final int WINDOW_HEIGHT = 1080;
+    private static final int WINDOW_WIDTH = 1600;
+    private static final int WINDOW_HEIGHT = 900;
 
     private Server server;
     private SpriteManager spriteManager;
